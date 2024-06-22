7feb74bc207112d0ecb8e4c947a504d0a8eb45a8
HcThanh-IT <93084511+HcThanh-IT@users.noreply.github.com>
2024-06-22 23:35:08 +0700

    Update 23:35 22/06/2024

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,7 +1,7 @@
 <div class="container-fluid position-relative p-0">
             <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
                 <a href="" class="navbar-brand p-0">
-                    <h1 class="m-0"><i class="fa fa-user me-3"></i>21661054</h1>
+                    <h1 class="m-0"><i class="fa fa-user me-3"></i>22661026</h1>
                     <!-- <img src="img/logo.png" alt="Logo"> -->
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
